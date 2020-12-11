1fb741ec885a34e6306ef731bee9547d8028c487
= <=>
2020-12-11 22:39:43 +0300

    Corected indenting

--- comparison.py
+++ comparison.py
@@ -1,7 +1,8 @@
 import timeit
 import random
-#code can take a while to finish if you want a faster runtime
-#either lower the length of the array or number of repeats in timeit function
+# code can take a while to finish if you want a faster runtime
+# either lower the length of the array or number of repeats in timeit function
+
 
 def compare_sort_speed(func_list, array):
     setup = """
@@ -13,19 +14,18 @@ from  selection_sort import selection_sort
 from  shell_sort import shell_sort
 """
     for func in func_list:
-        code = f"""
-{func}({array})
-"""
-        time = round(timeit.timeit(code, setup=setup, number=2),3) #decrease number of repeats here
+        code = f"{func}({array})"
+        # decrease number of repeats here
+        time = round(timeit.timeit(code, setup=setup, number=2), 3)
         print(f'sort time of {func} is: {time}')
 
 
 array = []
-for i in range(5000): # decrease array length here
-    array.append(random.randint(0,500))
+for i in range(5000):  # decrease array length here
+    array.append(random.randint(0, 500))
 
 
 func_list = ['bubble_sort_optimized', "bubble_sort_unoptimized",
-             "bubble_sort_cut_extra_loops","comb_sort", 'merge_sort',
-             "selection_sort", "shell_sort" ]
+             "bubble_sort_cut_extra_loops", "comb_sort", 'merge_sort',
+             "selection_sort", "shell_sort"]
 compare_sort_speed(func_list, array)
